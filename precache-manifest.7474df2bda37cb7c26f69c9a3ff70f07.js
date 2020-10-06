703237b59472f67c16ea7f8c2d4ca2582b8058b3
Mike <michael.guzman.personal@gmail.com>
2020-10-06 14:01:24 -0500

    Updates

---
+++ precache-manifest.7474df2bda37cb7c26f69c9a3ff70f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6ad014316b1fbd1942a5f2b3f24234",
+    "url": "/mytodo/index.html"
+  },
+  {
+    "revision": "a9264f2efec652e750ce",
+    "url": "/mytodo/static/css/main.2db844d7.chunk.css"
+  },
+  {
+    "revision": "08872349b6a799db2e91",
+    "url": "/mytodo/static/js/2.16242b50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodo/static/js/2.16242b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9264f2efec652e750ce",
+    "url": "/mytodo/static/js/main.4f0c4576.chunk.js"
+  },
+  {
+    "revision": "8d817f2f5f06f6f3220b",
+    "url": "/mytodo/static/js/runtime-main.4be00ac2.js"
+  }
+]);
